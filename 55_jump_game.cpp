61e3f9493aaa8cd0683b27b5b0887eec8022b1d0
psychehose <psychehose@gmail.com>
2025-01-13 00:40:54 +0900

    55

---
+++ 55_jump_game.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        if (nums.size() == 1) return true;
+
+        vector<bool> visited(nums.size(), false);
+
+        // max, cur size
+        queue<pair<int, int>> q;
+
+        q.push({nums[0], 0});
+        visited[0] = true;
+
+        while (!q.empty()) {
+            int max = q.front().first;
+            int cur = q.front().second;
+            q.pop();
+
+            if (max == 0) continue;
+            if (cur == nums.size() - 1 || cur + max >= nums.size() - 1) return true;            
+
+            for (int i = cur + 1; i < cur + max + 1; ++i) {
+                if (!visited[i]) {
+                    q.push({nums[i], i});
+                    visited[i] = true;
+                }
+            }
+            
+        }
+        return false;
+    }
+};
