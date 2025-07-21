5476fbfe177117758e95ec8947c09cec2421cfc6
psychehose <psychehose@gmail.com>
2025-07-22 08:15:55 +0900

    update solved.ac 2

---
+++ solved.ac_class2/boj11650.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <ios>
+#include <iostream>
+#include <utility>
+#include <vector>
+int main() {
+
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  int n;
+  std::cin >> n;
+  std::vector<std::pair<int, int>> vec(n);
+
+  for (int i = 0; i < n; ++i) {
+    std::cin >> vec[i].first >> vec[i].second;
+  }
+
+  std::sort(vec.begin(), vec.end(),
+            [](std::pair<int, int> p1, std::pair<int, int> p2) {
+              if (p1.first == p2.first) {
+                return p1.second < p2.second;
+              }
+              return p1.first < p2.first;
+            });
+
+  for (const auto &el : vec) {
+    std::cout << el.first << " " << el.second << "\n";
+  }
+
+  return 0;
+}
